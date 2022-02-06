54c36c74bbf3d5079d2adc17a688a4b27115f11d
art <voland0@gmail.com>
2022-02-07 01:56:16 +0800

    Добавьте файлы проекта.

---
+++ Employees/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Employees.ViewModels.Base;
+
+
+namespace Employees.ViewModels
+
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        #region Заголовок окна
+
+        private string _Title = "Работники";
+
+        /// <summary>Заголовок окна</summary>
+        public string Title
+        {
+            get => _Title;
+            //set
+            //{
+            //    //if (Equals(_Title, value)) return;
+            //    //_Title = value;
+            //    //OnPropertyChanged();
+
+            //    Set(ref _Title, value);
+            //}
+            set => Set(ref _Title, value);
+        }
+
+        #endregion
+    }
+}
